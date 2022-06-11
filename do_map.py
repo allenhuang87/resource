72315c0b6067692b887ee3b5e8f1581941327753
allenhuang87 <allenhuang87@163.com>
2022-06-11 17:35:51 +0800

    Update do_map.py

--- do_map.py
+++ do_map.py
@@ -28,3 +28,11 @@ def quadratic(a, b, c):
         return x,y
     else:
         return 'no answer!'
+
+#利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法：
+def trim(s):
+    while(s[:1]==' '):
+        s=s[1:]
+    while(s[-1:] == ' '):
+        s = s[:-1]
+    return s
