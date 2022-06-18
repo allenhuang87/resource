d324eb2283030c793031e86f0fb703ce9258413d
allenhuang87 <allenhuang87@163.com>
2022-06-18 11:06:19 +0800

    Update do_list.py

--- do_list.py
+++ do_list.py
@@ -1,2 +1,5 @@
-#/bin/env python
-
+#!/usr/bin/env python                                                                                                                                                   
+def do_list(m = 2,  n= 3): 
+    print(m, "+", n, "=",m+n)
+do_list()
+do_list(3,4)
